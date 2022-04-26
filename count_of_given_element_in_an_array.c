2b8f699bb508cc0e4aa83ddef8385c981fcd364e
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-04-26 23:32:27 +0530

    #include<stdio.h> int main() {     int n,arr[40],i,z;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&z);     int flag=0;     for(i=0;i<n;i++)     {         if(arr[i]==z)         {             flag++;            // break;         }     }     if(flag==0)     printf("%d",flag);     else     printf("%d",flag);     return 0; }

---
+++ count_of_given_element_in_an_array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[40],i,z;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&z);
+    int flag=0;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==z)
+        {
+            flag++;
+           // break;
+        }
+    }
+    if(flag==0)
+    printf("%d",flag);
+    else
+    printf("%d",flag);
+    return 0;
+}
