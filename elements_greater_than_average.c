235f264c6a513a36291573d4908bd49be31e9789
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-05-01 18:30:20 +0530

    #include<stdio.h> int main() {     int n,i,avg,s,x;     scanf("%d",&n);     int arr[50];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int add=0;     for(i=0;i<n;i++)     {         add=add+arr[i];         //avg=add/n;     }     avg=float(add)/float(n);     s=0;     for(i=0;i<n;i++)     {         //s=arr[0];         if(avg<=arr[i])         {             s++;         }     }     printf("%d",s);     return 0; }

---
+++ elements_greater_than_average.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,avg,s,x;
+    scanf("%d",&n);
+    int arr[50];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int add=0;
+    for(i=0;i<n;i++)
+    {
+        add=add+arr[i];
+        //avg=add/n;
+    }
+    avg=float(add)/float(n);
+    s=0;
+    for(i=0;i<n;i++)
+    {
+        //s=arr[0];
+        if(avg<=arr[i])
+        {
+            s++;
+        }
+    }
+    printf("%d",s);
+    return 0;
+}
