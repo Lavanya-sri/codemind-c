0ed50f7ff79e916ec64558c2f4bb738e26195101
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-06-28 20:12:44 +0530

    #include<stdio.h> int main() {     int n,m,i,j,max,arr[100][100];     scanf("%d%d",&n,&m);     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&arr[i][j]);         }     }     for(j=0;j<m;j++)     {         max=0;         for(i=0;i<n;i++)         {             if(arr[i][j]>max)             {             max=arr[i][j];             }         }         printf("%d ",max);     }     return 0; }

---
+++ Maximum_element_in_each_column.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,i,j,max,arr[100][100];
+    scanf("%d%d",&n,&m);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(j=0;j<m;j++)
+    {
+        max=0;
+        for(i=0;i<n;i++)
+        {
+            if(arr[i][j]>max)
+            {
+            max=arr[i][j];
+            }
+        }
+        printf("%d
+",max);
+    }
+    return 0;
+}
