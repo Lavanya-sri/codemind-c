3d772fe7cc29f7eeb052f4a51501b7b45bc52784
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-06-29 19:56:08 +0530

    #include<stdio.h> int main() {     int i,n,m,temp=0,c,r,arr[100];     scanf("%d%d",&n,&m);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=n;i<=m;i++)     {         temp=i;         c=0;         while(temp!=0)         {             r=temp%10;             if(r==0||i%r!=0)             {                 c=1;                 break;             }             temp=temp/10;         }         if(c==0)         {             printf("%d ",i);         }     }     return 0; }

--- Self_Dividing_Numbers.c
+++ Self_Dividing_Numbers.c
@@ -1,21 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int i,r,temp,a,b;
-    scanf("%d %d",&a,&b);
-    for(i=a;i<=b;i++)
+    int i,n,m,temp=0,c,r,arr[100];
+    scanf("%d%d",&n,&m);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=n;i<=m;i++)
     {
         temp=i;
-        while(temp>0)
+        c=0;
+        while(temp!=0)
         {
             r=temp%10;
             if(r==0||i%r!=0)
             {
+                c=1;
                 break;
             }
             temp=temp/10;
         }
-        if(temp==0)
+        if(c==0)
         {
             printf("%d ",i);
         }
