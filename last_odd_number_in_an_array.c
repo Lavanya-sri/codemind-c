84f595ac1764a25cf8c69b992605d2cbbfcf4af8
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-06-30 10:39:34 +0530

    #include<stdio.h> int main() {     int n,i,odd;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {            odd=arr[i];         }     }     printf("%d",odd);     return 0; }

--- last_odd_number_in_an_array.c
+++ last_odd_number_in_an_array.c
@@ -1,21 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[40],i;
+    int n,i,odd;
     scanf("%d",&n);
+    int arr[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    int odd=0;
-    for(i=n-1;i>=0;i--)
+    for(i=0;i<n;i++)
     {
         if(arr[i]%2!=0)
         {
-            odd=arr[i];
-            break;
+           odd=arr[i];
         }
     }
-    printf("%d",arr[i]);
+    printf("%d",odd);
     return 0;
 }
