49df18e7850cd6b63c9501db02dd5e8eca345f92
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-06-30 10:34:10 +0530

    #include<stdio.h> int main() {     int n,i,sum=0;     float avrg=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         sum=sum+arr[i];     }     avrg=(float(sum)/float(n));     printf("%.2f",avrg);     return 0; }

--- average_of_the_array.c
+++ average_of_the_array.c
@@ -1,20 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int n,arr[40],i;
+    int n,i,sum=0;
+    float avrg=0;
     scanf("%d",&n);
+    int arr[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    float sum=arr[0];
-    float c;
-    for(i=1;i<n;i++)
+    for(i=0;i<n;i++)
     {
         sum=sum+arr[i];
-        //c=sum/n;
     }
-    c=sum/n;
-    printf("%.2f",c);
+    avrg=(float(sum)/float(n));
+    printf("%.2f",avrg);
     return 0;
 }
