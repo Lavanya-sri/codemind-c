e9a0929ef6f65e2b2b079098d919b4c66583e9b0
Lavanya-sri <100119852+Lavanya-sri@users.noreply.github.com>
2022-07-01 09:52:46 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,arr[100],i,j,sum=0,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;       for(j=i+1;j<n;j++)       {           if(arr[i]==arr[j])           c++;       }       if(c==0&&arr[i]%2!=0)       {           sum++;       }     }     printf("%d",sum);     return 0;
    }

---
+++ odd_unique_elements_count.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,arr[100],i,j,sum=0,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+      for(j=i+1;j<n;j++)
+      {
+          if(arr[i]==arr[j])
+          c++;
+      }
+      if(c==0&&arr[i]%2!=0)
+      {
+          sum++;
+      }
+    }
+    printf("%d",sum);
+    return 0;
+
+}
